6fef62636b1f3f676857943e3a1bddf0b9ae1761
Nate <daviesnathanael45@gmail.com>
2025-11-11 19:45:49 +0000

    started theproject! got a window present and a draggable box. there is a headerfile i made for colours and other constants!

--- main.c
+++ main.c
@@ -1,7 +1,81 @@
 
 #include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
+#include "utils.h"
+
+TTF_Init();
+
+//initialising the font
+TTF_Font* font = TTF_OpenFont("fonts/font.ttf",24);
+
+//defining variables here
+int mouse_x , mouse_y;
+
+//this function just makes it easier to change the colour by letting me use my own from utils.h
+void SetColor(SDL_Renderer* r, SDL_Color c){
+    SDL_SetRenderDrawColor(r,c.r,c.g,c.b,c.a);
+}
 
 int main(){
+    SDL_Init(SDL_INIT_VIDEO);
 
+
+    //initiate window
+    SDL_Window* window = SDL_CreateWindow(
+        "Asset Creator",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        SCREENWIDTH,SCREENHEIGHT,
+        0
+    );
+
+    //create the render and start events
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1,0);
+    int running = 1;
+    SDL_Event event;
+
+    //crates elements
+    SDL_Rect object = {100,100,100,100};
+    SDL_Rect bottomBar = {0,SCREENHEIGHT-100,SCREENWIDTH,100};
+
+
+    //main loop of window
+    while (running){
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {running = 0;}
+        }
+        
+        //gets mouse pos and updates them
+        Uint32 buttons = SDL_GetMouseState(&mouse_x,&mouse_y);
+
+        if (buttons & SDL_BUTTON(SDL_BUTTON_LEFT)){
+            object.x = mouse_x;
+            object.y = mouse_y;
+        }
+
+        
+        //start drawing area
+        SetColor(renderer, GRAY);
+        SDL_RenderClear(renderer);
+
+        SetColor(renderer,BLACK);
+        SDL_RenderFillRect(renderer,&object);
+
+        //draw the bottom bar
+        SetColor(renderer, DARKGRAY);
+        SDL_RenderFillRect(renderer,&bottomBar);
+
+        //updates the display
+        SDL_RenderPresent(renderer);
+    }
+
+
+
+    // when ending program
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    TTF_CLoseFont(font);
+    SDL_Quit();
     return 0;
 }
---
+++ utils.h
@@ -0,0 +1,17 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include <SDL2/SDL.h>
+
+const int SCREENHEIGHT = 600;
+const int SCREENWIDTH = 800;
+
+const SDL_Color RED = {255,0,0,255};
+const SDL_Color GREEN = {0,255,0,255};
+const SDL_Color BLUE = {0,0,255,255};
+const SDL_Color BLACK ={0,0,0,255};
+const SDL_Color WHITE = {255,255,255,255};
+const SDL_Color GRAY = {180,180,180,255};
+const SDL_Color DARKGRAY = {120,120,120,255};
+
+
+#endif
